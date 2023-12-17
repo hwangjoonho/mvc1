47f7ded65e808b45be13003e0075f3646819ccc9
jhhwang <jhhwang@edu-ai.co.kr>
2023-12-18 07:58:01 +0900

    first commit

--- src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -7,14 +7,21 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class MappingController {
 
+    @RequestMapping({ "/hello-basic", "/hello-hello" })
+    public String helloBasic() {
+
+        log.info("helloBasic");
+        return "ok";
+    }
+
     /**
      * PathVariable 사용
      * 변수명이 같으면 생략 가능
-     *
+     * 활용 : 리소스 경로에 식별자를 넣는 스타일
      * @PathVariable("userId") String userId -> @PathVariable userId
      * /mapping/userA
      */
-    @RequestMapping("/mapping/{userId}")
+    @RequestMapping(value = "/mapping/{userId}")
 //    public String mappingPath(@PathVariable("userId") String data) {
 //        log.info("mappingPath userId={}"+data);
     public String mappingPath(@PathVariable String userId) {
@@ -59,12 +66,38 @@ public class MappingController {
         return "ok";
     }
 
-    @RequestMapping("/hello-basic")
-    public String helloBasic() {
-
-        log.info("helloBasic");
+    /**
+     * Content-Type 헤더 기반 추가 매핑 Media Type
+     * Content-Type : 브라우저에서 요청 보낼때 해당 형식으로 보내겠다는 속성
+     * consumes="application/json"
+     * consumes="!application/json"
+     * consumes="application/*"
+     * consumes="*\/*"
+     * MediaType.APPLICATION_JSON_VALUE
+     * consumes : java에서 받을때 특정형식만 받겠다는 뜻
+     *
+     */
+    @PostMapping(value = "/mapping-consume", consumes = "application/json")
+    public String mappingConsumes() {
+        log.info("mappingConsumes");
         return "ok";
     }
+
+    /**
+     * Accept 헤더 기반 Media Type
+     * Accept 헤더는 브라우저에서 응답(response) 받을때 특정형식만 받겠다는 속성
+     * produces = "text/html"
+     * produces = "!text/html" * produces = "text/*"
+     * produces = "*\/*"
+     * produces : java에서 response 보낼 때 해당 형식으로 보내겠다는 뜻
+     * 만약 맞지 않으면 HTTP 406 상태코드(Not Acceptable)을 반환
+     */
+    @PostMapping(value = "/mapping-produce", produces = "text/html")
+    public String mappingProduces() {
+        log.info("mappingProduces");
+        return "ok";
+    }
+
 
     /**
      * method 특정 HTTP 메서드 요청만 허용 * GET, HEAD, POST, PUT, PATCH, DELETE
